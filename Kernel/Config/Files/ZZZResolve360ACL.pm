771ce9b1a82cfa445514c7a61453bbc4bf867eaf
Digital Factory SN <df@digitalfactory.sn>
2026-09-08 10:17:35 -0400

    fix(acl): resolve array reference syntax in ZZZResolve360ACL.pm to eliminate 500 error

--- Kernel/Config/Files/ZZZResolve360ACL.pm
+++ Kernel/Config/Files/ZZZResolve360ACL.pm
@@ -8,23 +8,20 @@ use utf8;
 sub Load {
     my ($File, $Self) = @_;
 
-    # ACL-01: Restrict Financial Resolution / Approval state for high amount claims (> 100 000 FCFA)
-    # Only agents with the 'approbateurs' group can set ticket state to 'clos (résolu)' or 'clos (remboursé)' when amount > 100k
+    # ACL-01: Restrict Financial Resolution / Approval state for high amount claims (>= 100 000 FCFA)
+    # Only agents with 'approbateurs' group rw permission can set ticket state to 'clos (résolu)' or 'clos (remboursé)' when amount >= 100k
     $Self->{TicketAcl}->{'ACL-Resolve360-FinancialApproval-Restriction'} = {
         'PossibleNot' => {
             'Ticket' => {
                 'State' => ['clos (résolu)', 'clos (remboursé)', 'closed successful'],
             },
         },
-        'PossibleNotAdd' => {},
         'Properties' => {
             'User' => {
-                'Group_Responsibility' => {
-                    'approbateurs' => [ '0' ], # Agent does NOT belong to approbateurs group
-                },
+                'Group_rw' => ['[Not]approbateurs'],
             },
             'DynamicField' => {
-                'Resolve360_FinancialImpact' => ['[1-9][0-9]{5,}', '[1-9][0-9]{6,}'], # Amount >= 100,000
+                'Resolve360_FinancialImpact' => ['[1-9][0-9]{5,}', '[1-9][0-9]{6,}'],
             },
         },
         'StopAfterMatch' => 0,
@@ -39,10 +36,7 @@ sub Load {
         },
         'Properties' => {
             'User' => {
-                'Group_Responsibility' => {
-                    'conformite' => [ '0' ],
-                    'approbateurs' => [ '0' ],
-                },
+                'Group_rw' => ['[Not]conformite', '[Not]approbateurs'],
             },
             'Queue' => {
                 'Name' => ['Conformité', 'Fraude'],
